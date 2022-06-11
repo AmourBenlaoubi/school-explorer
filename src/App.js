18d32386baa5e17a7f7ba493337ab1bd6af69cca
Amour Benlaoubi <a.benlaoubi@gmail.com>
2022-06-11 13:22:05 +0200

    title

--- src/App.js
+++ src/App.js
@@ -1,25 +1,15 @@
-import logo from './logo.svg';
-import './App.css';
+import { AppBar, Toolbar, Typography } from '@mui/material';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+const App = () => {
+    return (
+        <AppBar>
+            <Toolbar>
+                <Typography variant="h5" gutterBottom component="div">
+                    School Explorer
+                </Typography>
+            </Toolbar>
+        </AppBar>
+    );
+};
 
 export default App;
